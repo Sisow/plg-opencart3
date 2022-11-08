608a02fa72b92b96a4965720e7849fe0619f9dc8
Andrei Mihalescu <andreimihalescu07@gmail.com>
2022-11-08 17:43:43 +0200

    BP-1809 Sisow Rebranding

--- admin/language/en-gb/extension/payment/sisow.php
+++ admin/language/en-gb/extension/payment/sisow.php
@@ -1,38 +1,38 @@
 <?php
-$_['heading_title_sisowideal']		= 'Sisow iDEAL';
-$_['heading_title_sisowmc']			= 'Sisow Bancontact';
-$_['heading_title_sisowde']			= 'Sisow SofortBanking';
-$_['heading_title_sisowecare']		= 'Sisow ecare';
-$_['heading_title_sisowovb']		= 'Sisow OverBoeking';
-$_['heading_title_sisowwg']			= 'Sisow Webshop Giftcard';
-$_['heading_title_sisowfijn']		= 'Sisow Fijncadeau';
-$_['heading_title_sisowpp']			= 'Sisow PayPal';
-$_['heading_title_sisowklarna']		= 'Sisow Klarna';
-$_['heading_title_sisowmaestro']			= 'Sisow Maestro';
-$_['heading_title_sisowmastercard']			= 'Sisow MasterCard';
-$_['heading_title_sisowvisa']			= 'Sisow Visa';
-$_['heading_title_sisowmob']		= 'Sisow Mobile';
-$_['heading_title_sisowfocum']		= 'Sisow Focum Achteraf Betalen';
-$_['heading_title_sisoweps']		= 'Sisow EPS';
-$_['heading_title_sisowgiropay']	= 'Sisow Giropay';
-$_['heading_title_sisowhomepay']	= 'Sisow ING Home\'Pay';
-$_['heading_title_sisowvvv']	= 'Sisow VVV Giftcard';
-$_['heading_title_sisowbunq']	= 'Sisow bunq';
-$_['heading_title_sisowidealqr']	= 'Sisow iDEAL QR';
-$_['heading_title_sisowvpay']	= 'Sisow V PAY';
-$_['heading_title_sisowafterpay']	= 'Sisow Afterpay';
-$_['heading_title_sisowbelfius']	= 'Sisow Belfius Pay Button';
-$_['heading_title_sisowcapayable']	= 'Sisow In3 Gespreid Betalen';
-$_['heading_title_sisowcbc']	= 'Sisow CBC';
-$_['heading_title_sisowkbc']	= 'Sisow KBC';
-$_['heading_title_sisowbillink']	= 'Sisow Billink';
-$_['heading_title_sisowspraypay']	= 'Sisow Spraypay';
+$_['heading_title_sisowideal']		= 'iDEAL';
+$_['heading_title_sisowmc']			= 'Bancontact';
+$_['heading_title_sisowde']			= 'SofortBanking';
+$_['heading_title_sisowecare']		= 'ecare';
+$_['heading_title_sisowovb']		= 'OverBoeking';
+$_['heading_title_sisowwg']			= 'Webshop Giftcard';
+$_['heading_title_sisowfijn']		= 'Fijncadeau';
+$_['heading_title_sisowpp']			= 'PayPal';
+$_['heading_title_sisowklarna']		= 'Klarna';
+$_['heading_title_sisowmaestro']			= 'Maestro';
+$_['heading_title_sisowmastercard']			= 'MasterCard';
+$_['heading_title_sisowvisa']			= 'Visa';
+$_['heading_title_sisowmob']		= 'Mobile';
+$_['heading_title_sisowfocum']		= 'Focum Achteraf Betalen';
+$_['heading_title_sisoweps']		= 'EPS';
+$_['heading_title_sisowgiropay']	= 'Giropay';
+$_['heading_title_sisowhomepay']	= 'ING Home\'Pay';
+$_['heading_title_sisowvvv']	= 'VVV Giftcard';
+$_['heading_title_sisowbunq']	= 'bunq';
+$_['heading_title_sisowidealqr']	= 'iDEAL QR';
+$_['heading_title_sisowvpay']	= 'V PAY';
+$_['heading_title_sisowafterpay']	= 'Afterpay';
+$_['heading_title_sisowbelfius']	= 'Belfius Pay Button';
+$_['heading_title_sisowcapayable']	= 'In3 Gespreid Betalen';
+$_['heading_title_sisowcbc']	= 'CBC';
+$_['heading_title_sisowkbc']	= 'KBC';
+$_['heading_title_sisowbillink']	= 'Billink';
+$_['heading_title_sisowspraypay']	= 'Spraypay';
 
 // Text 
 $_['text_payment']			= 'Payment';
 $_['text_success']			= 'Success: your configuration has changed';
 $_['text_all_zones']		= 'All Zones';
-$_['text_sisow']			= '<a onclick="window.open(\'http://www.sisow.nl\');"><img src="view/image/payment/sisowklein.png" alt="Sisow" title="Sisow" style="border: 1px solid #EEEEEE;" /></a>';
+$_['text_sisow']			= '<a onclick="window.open(\'http://www.buckaroo.nl\');"><img src="view/image/payment/buckaroo.png" alt="Buckaroo" title="Buckaroo" style="border: 1px solid #EEEEEE;" /></a>';
 $_['text_sisowideal']		= $_['text_sisow'];
 $_['text_sisowmc']			= $_['text_sisow'];
 $_['text_sisowde']			= $_['text_sisow'];
@@ -65,12 +65,12 @@ $_['text_sisowspraypay']			= $_['text_sisow'];
 $_['text_version']			= 'Version 5.4.5 for Opencart 3.X';
 
 // Entry
-$_['entry_merchantid']		= 'Sisow Merchant ID';
-$_['entry_merchantkey']		= 'Sisow Merchant Key';
-$_['entry_shopid']			= 'Sisow ShopID';
+$_['entry_merchantid']		= 'Website Key<br/><span class="help">The website key can be found in the Buckaroo Plaza under → My Buckaroo → Websites. Using the filter button at the top right, you can then switch between multiple websites (if multiple websites are linked within the same Buckaroo account).</span>';
+$_['entry_merchantkey']		= 'Secret Key<br/><span class="help">Can be set in the Buckaroo Plaza at → Configuration → Security → Secret Key</span>';
+$_['entry_shopid']			= 'ShopID<br/><span class="help">Field is no longer mandatory </span>';
 $_['entry_success']			= 'Success Status';
 $_['entry_useb2b']			= 'Use B2B';
-$_['entry_testmode']		= 'Testmode';
+$_['entry_testmode']        = 'Testmode<br/><span class="help">If you want to test transactions</span>';
 $_['entry_total']			= 'Minimum Order Total';
 $_['entry_totalmax']		= 'Maximum Order Total';
 $_['entry_geo_zone']		= 'Geo Zone';
@@ -81,13 +81,13 @@ $_['entry_sort_order']		= 'Sort Order';
 $_['entry_version_status']	= 'Module version';
 $_['entry_author']			= 'Author';
 $_['entry_ovbprefix']		= 'Order number prefix';
-$_['entry_extra_contract']	= 'For this payment method an additional contract is needed, please contact <a href="mailto:sales@sisow.nl">sales@sisow.nl</a>.';
+$_['entry_extra_contract']  = 'This payment option requires an additional contract, please contact <a href="wecare@sisow.nl">wecare@sisow.nl</a>.';
 
-$_['entry_createinvoice']	= 'Make invoice on reservation<br/><span class="help">If "Yes", the invoice will be created immediately after the credit check.</span>';
+$_['entry_makeinvoice']     = 'Invoicing directly<br/><span class="help">If "Yes", then the Sisow ecare invoice is also created immediately and the payment term of 14 days starts.</span>';
 
 // Entry Sisow ecare
-$_['entry_makeinvoice']		= 'Instant Invoice';
-$_['entry_mailinvoice']		= 'Mail Invoice';
+$_['entry_makeinvoice']     = 'Invoicing directly<br/><span class="help">If "Yes", then the Sisow ecare invoice is also created immediately and the payment term of 14 days starts.</span>';
+$_['entry_mailinvoice']     = 'Email invoice<br/><span class="help">If "Yes", the Sisow ecare invoice will be sent via Sisow immediately after creation.</span>';
 $_['entry_paymentfee']		= 'Payment Fee';
 $_['entry_sisowecarefee_tax'] = 'Tax Class Payment Fee';
 
@@ -108,6 +108,6 @@ $_['entry_klarnapaymentfee']	= 'Payment Fee';
 
 // Error
 $_['error_permission']  	= 'Warning: You do not have permission to modify these settings!';
-$_['error_merchantid']  	= 'Sisow Merchant ID required!';
-$_['error_merchantkey'] 	= 'Sisow Merchant Key required!';
+$_['error_merchantid']  	= 'Merchant ID required!';
+$_['error_merchantkey'] 	= 'Merchant Key required!';
 ?>
